9a67b037a95b3a48d0c04a2f56622900478fc3cf
Nadilson <nadilson@protonmail.com>
2023-02-12 17:58:28 -0300

    Novos ajustes nos filtros (dados estáticos, ainda)

--- src/pages/RepositoriesPage/Filter/index.js
+++ src/pages/RepositoriesPage/Filter/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import { Container, Selector, Cleaner } from './styles';
 
-const Filter = () => {
-  const langs = [
-    { name: 'JavaScript', count: 5, color: '#f1c40f' },
-    { name: 'Shell', count: 2, color: '#95a5a6' },
-    { name: 'PHP', count: 1, color: '#3498db' },
-  ];
-
-  const selectors = langs.map(({ name, count, color }) => (
+const Filter = ({ languages }) => {
+  const selectors = languages.map(({ name, count, color }) => (
     <Selector key={name.toLowerCase()} color={color}>
       <span>{name}</span>
       <span>{count}</span>
@@ -22,6 +18,16 @@ const Filter = () => {
       <Cleaner>Limpar</Cleaner>
     </Container>
   );
+};
+
+Filter.propTypes = {
+  languages: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      count: PropTypes.number.isRequired,
+      color: PropTypes.string,
+    }).isRequired
+  ).isRequired,
 };
 
 export default Filter;
--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -5,6 +5,8 @@ import Profile from './Profile';
 import Filter from './Filter';
 import Repositories from './Repositories';
 
+import { getLangsFrom } from '../../services/api';
+
 const RepositoriesPage = () => {
   const user = {
     login: 'Nadilson-IFPE',
@@ -17,11 +19,71 @@ const RepositoriesPage = () => {
     location: 'Recife - Pernambuco',
   };
 
+  const repositories = [
+    {
+      name: 'Repo 1',
+      description: 'Descrição',
+      html_url: 'https://www.w3schools.com/js/',
+      language: 'JavaScript',
+    },
+    {
+      name: 'Repo 2',
+      description: 'Descrição',
+      html_url: 'https://www.w3schools.com/js/',
+      language: 'JavaScript',
+    },
+    {
+      name: 'Repo 3',
+      description: 'Descrição',
+      html_url: 'https://www.docker.com/',
+      language: 'Docker',
+    },
+    {
+      name: 'Repo 4',
+      description: 'Descrição',
+      html_url: 'https://nadportfolio.vercel.app/',
+      language: null,
+    },
+    {
+      name: 'Repo 5',
+      description: 'Descrição',
+      html_url: 'https://www.typescriptlang.org/',
+      language: 'TypeScript',
+    },
+    {
+      name: 'Repo 6',
+      description: 'Descrição',
+      html_url: 'https://www.ruby-lang.org/en/',
+      language: 'Ruby',
+    },
+    {
+      name: 'Repo 7',
+      description: 'Descrição',
+      html_url:
+        'https://docs.oracle.com/javase/8/docs/technotes/guides/language/index.html',
+      language: 'Java',
+    },
+    {
+      name: 'Repo 8',
+      description: 'Descrição',
+      html_url: 'https://kotlinlang.org/',
+      language: 'Kotlin',
+    },
+    {
+      name: 'Repo 9',
+      description: 'Descrição',
+      html_url: 'https://dart.dev/',
+      language: 'Dart',
+    },
+  ];
+
+  const languages = getLangsFrom(repositories);
+
   return (
     <Container>
       <Sidebar>
         <Profile user={user} />
-        <Filter />
+        <Filter languages={languages} />
       </Sidebar>
       <Main>
         <Repositories />
