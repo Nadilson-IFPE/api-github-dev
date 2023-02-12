a3d914ae5d07fe5fe0397fbc32dced6241b9e99f
Nadilson <nadilson@protonmail.com>
2023-02-12 16:56:59 -0300

    Tipagem no componente Profile (dados estáticos, ainda)

--- src/pages/RepositoriesPage/Profile/index.js
+++ src/pages/RepositoriesPage/Profile/index.js
@@ -2,32 +2,53 @@ import React from 'react';
 
 import { MdGroup, MdLocationCity, MdWork, MdLink } from 'react-icons/md';
 
+import PropTypes from 'prop-types';
 import { Container, Header, Avatar, Login, Name, Inner, Data } from './styles';
 
-const Profile = () => (
+const Profile = ({ user }) => (
   <Container>
     <Header>
-      <Avatar src="https://avatars.githubusercontent.com/u/11899797?v=4" />
-      <Login>Nadilson-IFPE</Login>
-      <Name>Nadilson</Name>
+      <Avatar src={user.avatar_url} />
+      <Login>{user.login}</Login>
+      <Name>{user.name}</Name>
     </Header>
     <Inner>
       <Data>
         <MdGroup size={20} />
-        48&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;70&nbsp;<i>seguindo</i>
+        {user.following}&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;
+        {user.followers}&nbsp;<i>seguindo</i>
       </Data>
-      <Data>
-        <MdWork size={20} /> [DESEMPREGADO]
-      </Data>
-      <Data>
-        <MdLocationCity size={20} /> Recife
-      </Data>
-      <Data>
-        <MdLink size={20} />
-        <a href="https://nadportfolio.vercel.app/">Site pessoal de Nadilson</a>
-      </Data>
+      {user.company && (
+        <Data>
+          <MdWork size={20} /> {user.company}
+        </Data>
+      )}
+      {user.location && (
+        <Data>
+          <MdLocationCity size={20} /> {user.location}
+        </Data>
+      )}
+      {user.blog && (
+        <Data>
+          <MdLink size={20} />
+          <a href={`\\${user.blog}`}>{user.blog}</a>
+        </Data>
+      )}
     </Inner>
   </Container>
 );
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    company: PropTypes.string,
+    blog: PropTypes.string,
+    location: PropTypes.string,
+  }).isRequired,
+};
 
 export default Profile;
--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -5,16 +5,29 @@ import Profile from './Profile';
 import Filter from './Filter';
 import Repositories from './Repositories';
 
-const RepositoriesPage = () => (
-  <Container>
-    <Sidebar>
-      <Profile />
-      <Filter />
-    </Sidebar>
-    <Main>
-      <Repositories />
-    </Main>
-  </Container>
-);
+const RepositoriesPage = () => {
+  const user = {
+    login: 'Nadilson-IFPE',
+    name: 'Nadilson',
+    avatar_url: 'https://avatars.githubusercontent.com/u/11899797?v=4',
+    followers: 48,
+    following: 70,
+    company: '[DESEMPREGADO]',
+    blog: 'https://nadportfolio.vercel.app/',
+    location: 'Recife - Pernambuco',
+  };
+
+  return (
+    <Container>
+      <Sidebar>
+        <Profile user={user} />
+        <Filter />
+      </Sidebar>
+      <Main>
+        <Repositories />
+      </Main>
+    </Container>
+  );
+};
 
 export default RepositoriesPage;
