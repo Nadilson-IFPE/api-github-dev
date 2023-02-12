d25d0bc7e948eebdfb178fde10cade26ea29edfe
Nadilson <nadilson@protonmail.com>
2023-02-12 13:57:17 -0300

    Filtros (dados estáticos, ainda)

---
+++ src/pages/RepositoriesPage/Filter/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Container, Selector, Cleaner } from './styles';
+
+const Filter = () => {
+  const langs = [
+    { name: 'JavaScript', count: 5, color: '#f1c40f' },
+    { name: 'Shell', count: 2, color: '#95a5a6' },
+    { name: 'PHP', count: 1, color: '#3498db' },
+  ];
+
+  const selectors = langs.map(({ name, count, color }) => (
+    <Selector key={name.toLowerCase()} color={color}>
+      <span>{name}</span>
+      <span>{count}</span>
+    </Selector>
+  ));
+
+  return (
+    <Container>
+      {selectors}
+      <Cleaner>Limpar</Cleaner>
+    </Container>
+  );
+};
+
+export default Filter;
--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Container, Sidebar, Main } from './styles';
 import Profile from './Profile';
+import Filter from './Filter';
 
 const RepositoriesPage = () => (
   <Container>
     <Sidebar>
       <Profile />
+      <Filter />
     </Sidebar>
     <Main>Main</Main>
   </Container>
